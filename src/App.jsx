d9ad2f288c737102257f810b506cd5a4b6208f93
muhammadamin-web <mirboqijonov@gmail.com>
2024-08-10 17:16:56 +0500

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Nav from "./Components/Nav/Nav"
 import Upcoming from "./Components/Upcoming/Upcoming"
 import img1 from './assets/images/intro1.png'
 import img2 from './assets/images/intro1.png'
+import Block_average from "./Components/Block_average/Block_average"
+import Houses_plan from "./Components/Houses_plan/Houses_plan"
 
 const App = () => {
   const images = [img1, img2];
@@ -17,8 +19,9 @@ const App = () => {
       <Upcoming images={images}/>
       <About/>
       <Location/>
-      <Location/>
+      <Block_average/>
       <Blocks_location/>
+      <Houses_plan/>
       <Construction/>
       <Company/>
       <Footer/>
