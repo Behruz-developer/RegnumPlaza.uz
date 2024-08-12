58f1023c28fa0936a776346ec2b2c337f0b8c965
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-12 21:47:34 +0500

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import About from "./Components/About/About"
-import Company from "./Components/Company/Company"
-import Construction from "./Components/Construction/Construction"
-import Footer from "./Components/Footer/Footer"
-import Blocks_location from "./Components/Blocks_location/Blocks_location"
-import Location from "./Components/Location/Location"
-import Nav from "./Components/Nav/Nav"
-import Upcoming from "./Components/Upcoming/Upcoming"
-import img1 from './assets/images/intro1.png'
-import img2 from './assets/images/intro1.png'
-import Send_form from "./Components/Send_form/Send_form"
-import Block_average from "./Components/Block_average/Block_average"
-import Houses_plan from "./Components/Houses_plan/Houses_plan"
+import About from "./Components/About/About";
+import Company from "./Components/Company/Company";
+import Construction from "./Components/Construction/Construction";
+import Footer from "./Components/Footer/Footer";
+import Blocks_location from "./Components/Blocks_location/Blocks_location";
+import Location from "./Components/Location/Location";
+import Nav from "./Components/Nav/Nav";
+import Upcoming from "./Components/Upcoming/Upcoming";
+import img1 from "./assets/images/intro1.png";
+import img2 from "./assets/images/intro1.png";
+import Send_form from "./Components/Send_form/Send_form";
+import Block_average from "./Components/Block_average/Block_average";
+import Houses_plan from "./Components/Houses_plan/Houses_plan";
+import { useEffect, useState } from "react";
+import Loading from "./Components/Loading/Loading";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setIsLoading(false);
+    });
+
+    return () => window.removeEventListener("load", () => setIsLoading(false));
+  }, []);
+
   const images = [img1, img2];
   return (
     <>
-      <Nav/>
-      <Upcoming images={images}/>
-      <About/>
-      <Location/>
-      <Block_average/>
-      <Blocks_location/>
-      <Houses_plan/>
-      <Send_form/>
-      <Construction/>
-      <Company/>
-      <Footer/>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Nav />
+          <Upcoming images={images} />
+          <About />
+          <Location />
+          <Block_average />
+          <Blocks_location />
+          <Houses_plan />
+          <Send_form />
+          <Construction />
+          <Company />
+          <Footer />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
